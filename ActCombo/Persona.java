2ec1bac5ef6b9fc7f2bd7f008b3c658dc268f2d9
aletorresrios <123060901+aletorresrios@users.noreply.github.com>
2023-10-04 13:33:04 +0200

    Añadido actividad comboBox

---
+++ ActCombo/Persona.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desarrollointerfaces.ActCombo;
+
+/**
+ *
+ * @author Windows
+ */
+public class Persona {
+    String nombre;
+    String apellido;
+    int edad;
+    String telefono;
+    String email;
+
+    public Persona(String nombre, String apellido, int edad, String telefono, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nombre+" "+apellido;
+    }
+    
+}
